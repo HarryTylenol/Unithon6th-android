6ef4806a8d6ede92ad1107ed53615f70a7c183f4
박현기 <jungsanmango@gmail.com>
2018-01-27 17:30:52 +0900

    Speech Emmit

---
+++ app/src/main/java/team/unithon12/unithonteam12/util/UserInfo.kt
@@ -0,0 +1,11 @@
+package team.unithon12.unithonteam12.util
+
+/**
+ * Created by baghyeongi on 2018. 1. 27..
+ */
+object UserInfo {
+
+    var token : String? = null
+    var email : String? = null
+
+}
